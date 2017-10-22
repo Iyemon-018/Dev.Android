2b1a5281ab1133a1d78472774c10f8237d418849
イシイケンタ <iyemon018@gmail.com>
2017-10-22 16:29:31 +0900

    設定用サンプル プロジェクトを新規追加

--- ButterKnifeSample/app/src/main/java/com/example/butterknifesample/MainActivity.java
+++ ButterKnifeSample/app/src/main/java/com/example/butterknifesample/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.butterknifesample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton exampleRadioButton;
+    private Button exampleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        exampleRadioButton = (RadioButton) findViewById(R.id.exampleRadioButton);
+        exampleButton = (Button) findViewById(R.id.exampleButton);
+
     }
 }
